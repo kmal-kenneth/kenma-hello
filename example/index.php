638cd312ceb0ea0dfe060e98720f05b377c93a1a
Kenet Mauricio Acuña Lago <kmal.kenneth@gmail.com>
2020-03-17 12:59:11 -0600

    first commit

---
+++ example/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once "vendor/autoload.php";
+
+$hello = new \kenma\hello\hello();
+$hello->sayHello('php');
---
+++ src/hello.php
@@ -0,0 +1,10 @@
+<?php
+namespace kenma\hello;
+
+class Hello
+{
+    public function sayHello($message)
+    {
+        echo "Hello $message\n";
+    }
+}
